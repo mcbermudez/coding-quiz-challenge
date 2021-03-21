007ba79f8c6a8479232d42315362a1e5cdef029b
Mara <mara.bermudez@gmail.com>
2021-03-21 00:03:07 -0400

    initial files

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Create start button to start quiz
+
+// Start timer after page loads
+
+// Display questions to DOM (div/element/section - HTML5)
+
+// Display multiple choice options (attach to DOM - div/element/section - HTML5)
+
+// Attach time to DOM (see how much time remains  div/element/section - HTML5)
+
+// IF answered correctly --> next question
+
+// IF answered incorrectly --> subtract time clock
+
+// Create function to move to next question
+
+// Create function to deduct time from overall time
+
+// Game over function --> the last of the questions and/or time runs outs (track it by time?)
+
+// If question are answered --> game over --> stats ( if else/ if)
+
+// if timer reaches zer0 --> game over --> score
+
+// View score
